4df46a361614946483f215f27134be555d91f7e7
llAndyNicll <andynic.github@gmail.com>
2022-07-14 03:03:43 -0400

    Cambio de imagenes en miniatura agregado

--- src/componentes/tejidos/productos/ProductosCard.js
+++ src/componentes/tejidos/productos/ProductosCard.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b9004423d2bdb43ed90efd79697eec5dc3665a3063905e735b21fcfdb049e92
-size 7311
+oid sha256:a57a04d45f8e511bad5b3802c66457b7fab3714c47217485dc311b40883489a7
+size 9268
